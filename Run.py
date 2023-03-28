ccabe17fc7768703d6d9ed8dc448b9131756db71
Ηͥ͢Ꭿͣ͜ℕͫ͜Ѕ͢×፝֟͜× <77654205+Hansalrl@users.noreply.github.com>
2023-03-28 11:06:18 +0700

    Update Run.py
    Signed-off-by: Ηͥ͢Ꭿͣ͜ℕͫ͜Ѕ͢×፝֟͜× <77654205+Hansalrl@users.noreply.github.com>

--- Run.py
+++ Run.py
@@ -1,28 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 import random
-import os
 
 
 def change_url(option):
-    if option == '1':
-        url_dict = {
-            '1': {'url': 'https://www.facebook.com/login', 'name': 'Facebook'},
-            '2': {'url': 'https://www.instagram.com/accounts/login/', 'name': 'Instagram'},
-            '3': {'url': 'https://twitter.com/login', 'name': 'Twitter'},
-            '4': {'url': 'https://accounts.google.com/signin/v2/identifier', 'name': 'Gmail'},
-            '5': {'url': 'https://www.youtube.com/', 'name': 'Youtube'},
-            '6': {'url': 'https://discord.com/login', 'name': 'Discord'},
-            '7': {'url': 'https://web.telegram.org/k/', 'name': 'Telegram'}
-        }
-        try:
-            url = url_dict[option]['url']
-            print(f"Sedang menyiapkan web {url_dict[option]['name']}")
-            return url
-        except KeyError:
-            print("Maaf, pilihan tidak tersedia")
-            exit()
-    else:
+    url_dict = {
+        '1': {'url': 'https://www.facebook.com/login', 'name': 'Facebook'},
+        '2': {'url': 'https://www.instagram.com/accounts/login/', 'name': 'Instagram'},
+        '3': {'url': 'https://twitter.com/login', 'name': 'Twitter'},
+        '4': {'url': 'https://accounts.google.com/signin/v2/identifier', 'name': 'Gmail'},
+        '5': {'url': 'https://www.youtube.com/', 'name': 'Youtube'},
+        '6': {'url': 'https://discord.com/login', 'name': 'Discord'},
+        '7': {'url': 'https://web.telegram.org/k/', 'name': 'Telegram'}
+    }
+    try:
+        url = url_dict[option]['url']
+        print(f"Sedang menyiapkan web {url_dict[option]['name']}")
+        return url
+    except KeyError:
         print("Maaf, pilihan tidak tersedia")
         exit()
 
@@ -43,14 +38,25 @@ def main():
 
     session = requests.Session()
     response = session.get(url)
-    
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup)
-    token = soup.find('input', {'name': 'authenticity_token'}).get('value')
+    token_input = soup.find('input', {'name': 'authenticity_token'})
+    if token_input is not None:
+        token = token_input.get('value')
+    else:
+        print_colored_text("Tidak dapat menemukan authenticity_token")
+        exit()
+
+    username = input("Masukkan username atau email: ")
+    password = input("Masukkan password: ")
+
+    if not username or not password:
+        print_colored_text("Username atau password tidak boleh kosong")
+        exit()
 
     data = {
-        'session[username_or_email]': 'your_username_or_email',
-        'session[password]': 'your_password',
+        'session[username_or_email]': username,
+        'session[password]': password,
         'authenticity_token': token
     }
 
@@ -64,7 +70,7 @@ def main():
             for key, value in cookies.items():
                 f.write('%s:%s\n' % (key, value))
     else:
-        print_colored_text(f'Gagal login ke {url_dict[url_option]["name"]}')
+        print_colored_text(f'Gagal login ke {url_option}')
         with open('result/coki&tokengagal.txt', 'w') as f:
             for key, value in cookies.items():
                 f.write('%s:%s\n' % (key, value))
